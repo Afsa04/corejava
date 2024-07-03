a042c46fec34daa61e3ce460890c98e85372854b
DELL <DELL@DESKTOP-K1JR4PD>
2024-07-03 14:21:27 +0530

    commit

---
+++ corejavaprograms/src/com/tnsif/Packagetwo/Display.java
@@ -0,0 +1,10 @@
+package com.tnsif.Packagetwo;
+
+import com.tnsif.packages.Publicdemo;
+
+public class Display {
+	 public static void main(String[] args) {
+		Publicdemo p=new Publicdemo();
+		System.out.println(p.a);
+	 }
+}
